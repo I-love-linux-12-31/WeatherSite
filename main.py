ace2e6b0f1fdb0f5b2f725a52be873e6166c2508
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-09-01 22:15:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import secrets
+import datetime
+
+from flask import Flask
+import flask
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = secrets.token_hex()
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.send_file("./Frontend/index.html")
+
+
+@app.route("/favicon.svg")
+def icon():
+    return flask.send_file("./Frontend/favicon.svg")
+
+
+@app.route("/style.css")
+def style():
+    return flask.send_file("./Frontend/style.css")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host="0.0.0.0")
