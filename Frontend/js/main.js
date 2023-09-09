882514e5a2ff0976117981a9e3b6e9cc07f4e22e
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-09-09 15:43:48 +0300

    Working on page footer and weather cards.

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,1 +1,11 @@
 console.log("JS working.")
+
+/*eckbox = document.getElementById("lang-select-checkbox");
+	if (checkbox != null){
+		checkbox.addEventListener('change', () => {
+		  console.log("Состояние чекбокса изменнено")
+		})
+	}
+	else {
+		  console.log("Error")
+	}*/
