a52d15f400dcff398c544d6a94a78235e6283755
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-09-23 14:25:45 +0300

    23_09_2023

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -9,3 +9,27 @@ const errorCallback = (error) => {
 };
 
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('http://ipwho.is/',
+function(err, data) {
+  if (err !== null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    alert('Your city: ' + data["city"]);
+  }
+});
