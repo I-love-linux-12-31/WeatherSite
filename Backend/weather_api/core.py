aaf5bba57e537c4fac3038124f3723412f12c103
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-10-10 23:15:05 +0300

    Working on ui of weather cards and geolocation.

---
+++ Backend/weather_api/core.py
@@ -0,0 +1,6 @@
+import flask
+from flask.blueprints import Blueprint
+
+
+blueprint = Blueprint("Tasks backend", __name__)
+
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,16 +1,21 @@
 console.log("JS working.")
 
 const successCallback = (position) => {
-  console.log(position);
+  // console.log(position);
+  // console.log("!!!", location_input)
+  // alert('Your position [navigator]: ' + position.coords.latitude + ';' + position.coords.longitude);
+  // location_input.setAttribute("value", "" + position.coords.latitude + ';' + position.coords.latitude);
+  location_input.value = "" + position.coords.latitude + ';' + position.coords.longitude;
+  // location_input.innerText = "" + position.coords.latitude + ';' + position.coords.latitude;
 };
 
 const errorCallback = (error) => {
   console.log(error);
 };
 
-/* // Disabled geolocation todo: make gui
+ // Disabled geolocation todo: make gui
 navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
+/*
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
