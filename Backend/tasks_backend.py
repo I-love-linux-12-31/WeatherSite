b8b6b8f4d79265c176a56218a67ae7b0e4364fcd
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-11-01 21:40:52 +0300

    Working on task 8

--- Backend/tasks_backend.py
+++ Backend/tasks_backend.py
@@ -19,3 +19,15 @@ def tasks():
 def webm():
     print("NOT INCLUDE TO FINAL SITE !!!")
     return flask.send_file("./Frontend/1.webm")
+
+
+@blueprint.route("/css/tasks_2.css")
+def tasks_2_css():
+    print("NOT INCLUDE TO FINAL SITE !!!")
+    return flask.send_file("./Frontend/css/tasks_2.css")
+
+
+@blueprint.route("/tasks/tasks_2.html")
+def tasks_2_html():
+    print("NOT INCLUDE TO FINAL SITE !!!")
+    return flask.send_file("./Frontend/tasks_page_2.html")
