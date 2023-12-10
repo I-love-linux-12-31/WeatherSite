2f4b6744c382ccd5901604509193554ed7e89b0b
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2023-12-10 14:51:14 +0300

    fixes

--- Frontend/js/themes.js
+++ Frontend/js/themes.js
@@ -1,16 +1,7 @@
 function change_theme() {
   console.log('Change theme!');
-/*  for ( i=0; i<document.styleSheets.length; i++) {
-    void(document.styleSheets.item(i).disabled=true);
-}*/
-/*var linkNode = document.getElementsByTagName('link')[2];
-linkNode.parentNode.removeChild(linkNode);*/
-
-  // var sheet = document.styleSheets[0];
-  // sheet.disabled = !sheet.disabled;
 
   var sheet = document.styleSheets[1];
   sheet.disabled = !sheet.disabled;
-
 
 }
